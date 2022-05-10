06b33d8a717e7610e71df4ed1302e0eb8b97f03e
AryaNurarif112 <aryanurarrif@gmail.com>
2022-05-10 13:58:44 +0700

    Submission 5

---
+++ src/scripts/index.js
@@ -0,0 +1,37 @@
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.scss";
+import data from "../DATA.json";
+
+const menu_hamburger = document.querySelector(".menu_hamburger");
+const nav = document.querySelector("nav");
+menu_hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+const items_container = document.querySelector(".items_container");
+
+data.restaurants.forEach((restaurant) => {
+  const description =
+    restaurant.description.slice(0, 250) +
+    (restaurant.description.length > 250 ? "..." : "");
+  items_container.innerHTML += ` 
+    <div class="item">
+        <img src="${restaurant.pictureId}" alt="" />
+        <div class="text_item">
+        <h4>
+           ${restaurant.name}
+            <span class="rating">
+            <ion-icon name="star"></ion-icon>
+            <span>${restaurant.rating}</span>
+            </span>
+        </h4>
+        <p class="location">
+            <ion-icon name="location"></ion-icon>
+            <span>${restaurant.city}</span>
+        </p>
+        <p class="deskripsi">
+           ${description}
+        </p>
+        </div>
+    </div>`;
+});
